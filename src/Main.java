8cd3a65eb3b872ccfdc838c295617f2967b22610
brunkovskyi <brunkovskyi@ngoar.net>
2017-12-10 12:42:56 +0200

    Update mmm

--- src/Main.java
+++ src/Main.java
@@ -19,6 +19,7 @@ public class Main {
         }
 
         System.out.println("feature/c215-cascade2c3c4c5c6c--3334");
+        System.out.println("feature/c215-cascade2c3c4c5c6c--3334");
 
         System.out.println(first.getSeconds());
         System.out.println(nameThird + " : " + nameSecond);
